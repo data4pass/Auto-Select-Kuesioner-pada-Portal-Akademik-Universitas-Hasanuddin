70225f11bdf6dcb3c03c9f304111fd430414b42d
data4pass <bram.neutral@gmail.com>
2017-06-03 00:59:10 +0800

    Add files via upload

---
+++ AutoSelectKuesionerUNHAS.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Auto-Select Kuesioner untuk Portal Akademik Universitas Hasanuddin
+// @namespace    https://github.com/data4pass/
+// @version      0.1
+// @description  Automatically Answers Portal Akademik questionnaires for Universitas Hasanuddin students.
+// @author       data4pass
+// @match        https://*.unhas.ac.id/kuisioner/index.php/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var menu = document.getElementsByClassName("form-control");
+	for (var i = 0, l = menu.length; i < l; i++) {
+    	menu[i].selectedIndex = 3;
+	}
+    
+    document.getElementsByClassName("btn btn-primary btn-lg pull-right")[0].click();
+})();
